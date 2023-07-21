9ab752aee2ea6c41e839cdd1f703426585858361
lana2-2karla <allanakarla1702@gmail.com>
2023-07-21 19:19:26 -0300

    :tada: starting first project - Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            string? OneValue = Console.ReadLine();
+
+            Console.WriteLine("Segundo valor: ");
+            string? SecondValue = Console.ReadLine();
+             
+            // retorna true se o valor for nulo ou uma string vazia
+            if (!string.IsNullOrWhiteSpace(OneValue) && !string.IsNullOrWhiteSpace(SecondValue))
+            {
+                float OneValueNumber = float.Parse(OneValue);
+                float SecondValueNumber = float.Parse(SecondValue);
+               
+                float Summation = OneValueNumber + SecondValueNumber;
+
+                Console.WriteLine($"O resultado da soma é: {Summation}");
+            }
+
+            
+        }
+    }
+    
+}
