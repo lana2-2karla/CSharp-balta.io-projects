0e588937b69e95b4c2c835dd73483c915ad057b5
lana2-2karla <allanakarla1702@gmail.com>
2023-07-22 16:51:45 -0300

    :sparkles: feat: implements logic for minutes and seconds

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -11,10 +11,34 @@
 
             Console.Clear();
 
-            Console.WriteLine("S = Segundo");
-            Console.WriteLine("M = Minuto");
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deseja contar?");
+
+            string? data = Console.ReadLine();
+
+            if (!String.IsNullOrEmpty(data))
+            {
+                data = data.ToLower();
+                // Substring --> Pega a quantidade de caracteres da posição informada
+                char type = char.Parse(data.Substring(data.Length -1, 1));
+                int time = int.Parse(data.Substring(0, data.Length -1));
+
+                int multiplier = 1;
+
+                if (type == 'm')
+                    multiplier = 60;
+                
+                if (time == 0)
+                    System.Environment.Exit(0);
+
+                Start(time * multiplier);
+                
+            }
+                
+
+            
         }
 
         static void Start(int time)
@@ -32,7 +56,8 @@
 
             Console.Clear();
             Console.WriteLine("StopWatch finalizado");
-            Thread.Sleep(2500);
+            Thread.Sleep(2000);
+            Menu();
         }
     }
 }
