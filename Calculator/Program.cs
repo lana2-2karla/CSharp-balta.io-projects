edec030fc9ae1305136aaa64e699d96f21c0fc75
lana2-2karla <allanakarla1702@gmail.com>
2023-07-22 12:58:06 -0300

    :sparkles: feat: function Subtracao

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -28,6 +28,32 @@
                 Console.WriteLine($"O resultado da soma é: {Summation}");
             }
         }
+    
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            string? oneValue = Console.ReadLine();
+
+            Console.WriteLine("Segundo valor: ");
+            string? secondValue = Console.ReadLine();
+
+            Console.WriteLine("");
+             
+            // retorna true se o valor for nulo ou uma string vazia
+            if (!string.IsNullOrWhiteSpace(oneValue) && !string.IsNullOrWhiteSpace(secondValue))
+            {
+                float OneValueNumber = float.Parse(oneValue);
+                float SecondValueNumber = float.Parse(secondValue);
+               
+                float subtraction = OneValueNumber - SecondValueNumber;
+
+                Console.WriteLine($"O resultado da subtração é: {subtraction}");
+                Console.ReadKey();
+            }
+        }
+    
     }
     
 }
