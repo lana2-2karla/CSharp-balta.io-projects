945b7b6d590730dd1d1cf743a47b7cf566fa628e
lana2-2karla <allanakarla1702@gmail.com>
2023-07-22 15:43:04 -0300

    :sparkles: feat: function Start

---
+++ StopWatch/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace StopWatch
+{
+    class Program
+    {
+        static void Main()
+        {
+           Start();
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (time != currentTime)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                //Thread = execução atual --> Estamos pedindo para a execução "dormir" por 1 segundo
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
