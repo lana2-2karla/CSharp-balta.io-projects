c45c0641a5d2115b0f418cd88f1057dd697b4b99
lana2-2karla <allanakarla1702@gmail.com>
2023-07-22 15:52:43 -0300

    :sparkles: feat: function Menu

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -4,12 +4,21 @@
     {
         static void Main()
         {
-           Start();
+           Menu();
         }
 
-        static void Start()
+        static void Menu() {
+
+            Console.Clear();
+
+            Console.WriteLine("S = Segundo");
+            Console.WriteLine("M = Minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+        }
+
+        static void Start(int time)
         {
-            int time = 10;
             int currentTime = 0;
 
             while (time != currentTime)
@@ -20,6 +29,10 @@
                 //Thread = execução atual --> Estamos pedindo para a execução "dormir" por 1 segundo
                 Thread.Sleep(1000);
             }
+
+            Console.Clear();
+            Console.WriteLine("StopWatch finalizado");
+            Thread.Sleep(2500);
         }
     }
 }
