6af73b8a018201e1c9fb747b26796966f0e29c1b
lana2-2karla <allanakarla1702@gmail.com>
2023-07-22 13:01:43 -0300

    :sparkles: feat: the program does not end after the sum

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -16,6 +16,8 @@
 
             Console.WriteLine("Segundo valor: ");
             string? secondValue = Console.ReadLine();
+
+            Console.WriteLine("");
              
             // retorna true se o valor for nulo ou uma string vazia
             if (!string.IsNullOrWhiteSpace(oneValue) && !string.IsNullOrWhiteSpace(secondValue))
@@ -26,6 +28,7 @@
                 float Summation = OneValueNumber + SecondValueNumber;
 
                 Console.WriteLine($"O resultado da soma é: {Summation}");
+                Console.ReadKey();
             }
         }
     
