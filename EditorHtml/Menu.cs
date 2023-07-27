53dcb88c42fb3f7720830328088c34f46569ed95
lana2-2karla <allanakarla1702@gmail.com>
2023-07-27 17:32:30 -0300

    :sparkles: feat: function DrawScreen

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -7,8 +7,42 @@ namespace EditorHtml
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
-            
 
+            DrawScreen();
+        }
+
+        public static void DrawScreen()
+        {
+            OpeningAndClosing();
+
+            for (int lines = 0; lines <= 10; lines++)
+                Body();
+
+            OpeningAndClosing();
+        }
+    
+        public static void OpeningAndClosing()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+        }
+    
+        public static void Body()
+        {
+            Console.Write("|");
+
+                for (int i = 0; i <= 30; i++)
+                {
+                Console.Write(" ");
+                }
+
+                Console.Write("|");
+                Console.Write("\n");
         }
     }
 }
