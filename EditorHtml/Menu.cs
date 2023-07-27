ad22f5f8a32200440d3d0ad596c28b1c977373f3
lana2-2karla <allanakarla1702@gmail.com>
2023-07-27 17:59:28 -0300

    :sparkles: feat: WriteOptions

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -9,6 +9,14 @@ namespace EditorHtml
             Console.ForegroundColor = ConsoleColor.Black;
 
             DrawScreen();
+            WriteOptions();
+
+            string? option = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(option))
+            {
+                short optionShort = short.Parse(option);
+            }
         }
 
         public static void DrawScreen()
@@ -40,6 +48,30 @@ namespace EditorHtml
 
             Console.Write("|");
             Console.Write("\n");
+        }
+    
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3,2); // coluna 3, linha 2
+            Console.WriteLine("Editor HTML");
+
+            Console.SetCursorPosition(3,3);
+            Console.WriteLine("==========");
+
+            Console.SetCursorPosition(3,4);
+            Console.WriteLine("Selecione uma opção abaixo:");
+
+            Console.SetCursorPosition(3,6);
+            Console.WriteLine("1 - Novo arquivo");
+
+            Console.SetCursorPosition(3,7);
+            Console.WriteLine("2 - Abrir");
+
+            Console.SetCursorPosition(3,9);
+            Console.WriteLine("0 - Sair");
+
+            Console.SetCursorPosition(3,10);
+            Console.Write("Opção: ");
         }
     }
 }
