335120e21de63550ef1681669a20949a9d8be538
lana2-2karla <allanakarla1702@gmail.com>
2023-07-30 16:25:27 -0300

    :sparkles: feat: function HandleMenuOption

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -16,7 +16,9 @@ namespace EditorHtml
             if (!string.IsNullOrEmpty(option))
             {
                 short optionShort = short.Parse(option);
+                HandleMenuOption(optionShort);
             }
+
         }
 
         public static void DrawScreen()
@@ -72,6 +74,21 @@ namespace EditorHtml
 
             Console.SetCursorPosition(3,10);
             Console.Write("Opção: ");
+        }
+    
+        public static void HandleMenuOption(short option)
+        {
+            switch (option)
+            {
+                case 1: Console.WriteLine("Editor"); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0: {
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                }
+                default: Show(); break;
+            }
         }
     }
 }
