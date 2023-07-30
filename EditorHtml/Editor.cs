74f23611f82af8dc8cd360990ce671ec2d9c4ab2
lana2-2karla <allanakarla1702@gmail.com>
2023-07-30 16:35:05 -0300

    :sparkles: feat: class Editor: function Show

---
+++ EditorHtml/Editor.cs
@@ -0,0 +1,21 @@
+namespace EditorHtml
+{
+    public static class Editor
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Modo Editor");
+            Console.WriteLine("----------");
+            Start();
+        }
+
+        public static void Start()
+        {
+            
+        }
+    }
+}
