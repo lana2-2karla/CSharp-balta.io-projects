c1d218c9668a34641749f6d59909ea163431edaf
lana2-2karla <allanakarla1702@gmail.com>
2023-07-30 16:53:43 -0300

    :sparkles: feat: class Viewer: function Show

---
+++ EditorHtml/Viewer.cs
@@ -0,0 +1,23 @@
+namespace EditorHtml
+{
+    public class Viewer
+    {
+        public static void Show(string text)
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("Modo de Visualização");
+            Console.WriteLine("----------");
+            Replace(text);
+            Console.WriteLine("----------");
+            Console.ReadKey();
+            Menu.Show();
+        }
+
+        public static void Replace(string text) {
+
+        }
+    }
+}
