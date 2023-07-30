8abcd2ebc748848d105ed64913a4bf0c42e4bfa5
lana2-2karla <allanakarla1702@gmail.com>
2023-07-30 17:32:53 -0300

    :sparkles: feat: function Replace

--- EditorHtml/Viewer.cs
+++ EditorHtml/Viewer.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace EditorHtml
 {
     public class Viewer
@@ -11,13 +13,36 @@ namespace EditorHtml
             Console.WriteLine("Modo de Visualização");
             Console.WriteLine("----------");
             Replace(text);
+            Console.Write(" ");
             Console.WriteLine("----------");
             Console.ReadKey();
             Menu.Show();
         }
 
-        public static void Replace(string text) {
-
+         public static void Replace(string text)
+        {
+            var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
+            var words = text.Split(' ');
+            for (var i = 0; i < words.Length; i++)
+            {
+                if (strong.IsMatch(words[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(
+                        words[i].Substring(
+                            words[i].IndexOf('>') + 1,
+                            ((words[i].LastIndexOf('<') - 1) - words[i].IndexOf('>'))
+                        )
+                    );
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(words[i]);
+                    Console.Write(" ");
+                }
+            }
         }
     }
 }
